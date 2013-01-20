bed1595a971d9f78ce689b83666565d5c25d6e7b
Jim Kutter <jim@kuttergroup.com>
2013-01-20 09:44:40 -0500

    add .gitignore to files manually added to archive

--- zipit.php
+++ zipit.php
@@ -3,6 +3,7 @@ set_time_limit(300);
 
 $filename = md5(time()) . '.zip';
 system('zip -r '.$filename.' *'); 
-system('zip '.$filename.' .htaccess'); // hidden files too 
+system('zip '.$filename.' .htaccess'); 
+system('zip '.$filename.' .gitignore');
 
 unlink(__FILE__);
